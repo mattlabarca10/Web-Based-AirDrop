b0fed3f46333a6111e75a9b87aefa1276dd0cd42
Matthew LaBarca <matlab0510@gmail.com>
2024-01-24 02:06:20 -0500

    fixed firebase connection

--- app/firebase.js
+++ app/firebase.js
@@ -1,18 +1,20 @@
-// firebase.js
+
 import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCrudXhT5ZQSYHs382LF5lyXV6GA3-yKtM",
-    authDomain: "web-based-airdrop.firebaseapp.com",
-    databaseURL: "https://web-based-airdrop-default-rtdb.firebaseio.com",
-    projectId: "web-based-airdrop",
-    storageBucket: "web-based-airdrop.appspot.com",
-    messagingSenderId: "147452532990",
-    appId: "1:147452532990:web:975d54e8e4d5613ad4adaf",
-    measurementId: "G-PS6GJB214P"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 export default app;
+
