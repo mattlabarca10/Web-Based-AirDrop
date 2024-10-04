9ddea99136ec4a9f45d0dbe78feae371fdd7932d
Pat Ging <104949346+patging@users.noreply.github.com>
2024-10-04 14:01:25 -0400

    Update firebase.js
    Removed accidental leakage.

--- app/src/firebase.js
+++ app/src/firebase.js
@@ -4,14 +4,14 @@ import { getFirestore, collection } from 'firebase/firestore'; // Adjusted impor
 import { getStorage } from 'firebase/storage'; // Adjusted import for Storage module
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCrudXhT5ZQSYHs382LF5lyXV6GA3-yKtM",
-  authDomain: "web-based-airdrop.firebaseapp.com",
-  databaseURL: "https://web-based-airdrop-default-rtdb.firebaseio.com",
-  projectId: "web-based-airdrop",
-  storageBucket: "web-based-airdrop.appspot.com",
-  messagingSenderId: "147452532990",
-  appId: "1:147452532990:web:975d54e8e4d5613ad4adaf",
-  measurementId: "G-PS6GJB214P"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
